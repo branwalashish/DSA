6b6542bd814b59335183e6fc58158d43f1b14e85
Ashish kumar <72202312+branwalashish@users.noreply.github.com>
2024-09-05 11:02:38 +0530

    Update MyCircularLinkedList.java

--- linkedlistOperation/MyCircularLinkedList.java
+++ linkedlistOperation/MyCircularLinkedList.java
@@ -1,120 +1,176 @@
 package linkedlistOperation;
-
-
+/*
+ * Singly Circular LinkedList ==> It is a type of linked list where the last node’s next pointer refers back to 
+ * the first node, creating a circular connection. Unlike a regular Singly LinkedList, 
+ * there is no null at the end.
+ */
 public class MyCircularLinkedList {
-	class Node
-	{
-	int data;
-	Node next;
-	 
-	Node(int data){
-		this.data = data;
+
+	class Node {
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+		}
 	}
-}
+
 	Node head;
 	Node tail;
 	int c;
-	//it will add the element at last node
+
+	// It will add the element at the last node
 	public int add(int data) {
-		
 		Node newNode = new Node(data);
-		if(head == null) {
-			head =newNode;
+		if (head == null) {
+			head = newNode;
 			tail = newNode;
-			newNode.next = head;
+			newNode.next = head; 
 			c++;
-		}else {
+		} else {
 			tail.next = newNode;
-			tail = newNode;		
-			tail.next = head;
+			tail = newNode;
+			tail.next = head; 
 			c++;
 		}
 		return data;
 	}
-	//adding the node at first
+
+	// Adding the node at first
 	public int addFirst(int data) {
-		  Node newNode = new Node(data);
-		 
-		  if (head == null) {
-		    head = newNode;
-		    tail = newNode;
-		    newNode.next = head;
-		    
-		  } else {
-		    Node temp = head;
-		    newNode.next = temp;
-		    head = newNode;
-		    tail.next = head;
-		  }
-		  c++;
-		  return data;
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+			newNode.next = head; 
+		} else {
+			newNode.next = head;
+			head = newNode;
+			tail.next = head; 
 		}
-	//it will remove the first node
-	public int removeFirst() {
-		if(head==null) {
-			throw new NullPointerException("List is Empty");
-		}
-			int res = head.data;
-			if(head!=tail) // for more than one elements
-			{
-				head = head.next;
-				tail.next = head;
-			}else {
-				head=tail=null;
-			}
-			c--;
-			return res;	
+		c++;
+		return data;
 	}
-	//it will remove the last element
-	public int remove() {
-		if(head==null) {
+
+	// It will remove the first node
+	public int removeFirst() {
+		if (head == null) {
 			throw new NullPointerException("List is Empty");
 		}
-		int res = tail.data;
-		if(head!=tail) {
-			Node temp = head;
-			while(temp.next!=null) {
-				temp = temp.next;
-			}
-			tail = temp;
-			tail.next = head;
-		}else {
-			head=tail=null;
+		int res = head.data;
+		if (head != tail) { 
+			head = head.next;
+			tail.next = head; 
+		} else {
+			head = tail = null; 
 		}
 		c--;
 		return res;
 	}
-	
-//	public int remove(int index) {
-//		if(index<0||index>size()) {
-//			throw new IndexOutOfBoundsException("Index out of bound for this index"+index);
-//		}
-//		
-//		if(head==null) {
-//			throw new NullPointerException("Empty LinkedList");
-//		}
-//		Node temp = head;
-//		Node prev = null;
-//		
-//	}
-	
+
+	// It will remove the last element
+	public int remove() {
+		if (head == null) {
+			throw new NullPointerException("List is Empty");
+		}
+		int res = tail.data;
+
+		if (head != tail) { 
+			Node temp = head;
+			
+			while (temp.next != tail) { 
+				temp = temp.next;
+			}
+			tail = temp; 
+			tail.next = head; 
+		} else {
+			head = tail = null; 
+		}
+		c--;
+		return res;
+	}
+
+	// Remove element at specific index
+	public int remove(int index) {
+		if (index < 0 || index >= c) {
+			throw new IndexOutOfBoundsException("Index out of bound: " + index);
+		}
+
+		if (index == 0) { 
+			return removeFirst();
+		}
+
+		if (index == c - 1) { 
+			return remove();
+		}
+
+		Node temp = head;
+		for (int i = 0; i < index - 1; i++) { 
+			temp = temp.next;
+		}
+
+		int res = temp.next.data;
+		temp.next = temp.next.next;
+		c--;
+
+		return res;
+	}
+
+	// Display the elements in the list
 	public void display() {
 		Node temp = head;
-		if(head == null) {
+		if (head == null) {
 			System.out.println("List is Empty");
-		}
-		else {
+		} else {
 			do {
-				System.out.print(temp.data+"->");
+				System.out.print(temp.data + " -> ");
 				temp = temp.next;
-			}while(temp!=head);
+			} while (temp != head); // Stop when we return to the head
 		}
 		System.out.println();
 	}
-	
+
+	// Display elements in reverse (This is for illustrative purposes only, not a
+	// typical operation on a singly linked list)
+	public void displayReverse() {
+		if (head == null) {
+			System.out.println("List is Empty");
+			return;
+		}
+
+		Node temp = tail;
+		Node prev = null;
+		Node current = head;
+
+		// Reverse traversal using next-to-last node finding
+		for (int i = 0; i < c; i++) {
+			while (current.next != temp && current.next != head) {
+				current = current.next;
+			}
+			prev = current;
+			System.out.print(temp.data + " <- ");
+			temp = prev;
+			current = head;
+		}
+		System.out.println();
+	}
+
+	// Returns the size of the list
 	public int size() {
 		return c;
 	}
-	
-	
+
+	// Get element at specific index
+	public int get(int index) {
+		if (index < 0 || index >= c) {
+			throw new IndexOutOfBoundsException("Index out of bound: " + index);
+		}
+
+		Node temp = head;
+		for (int i = 0; i < index; i++) {
+			temp = temp.next;
+		}
+
+		return temp.data;
+	}
 }
